389d714b2b08f0eceeeb6a9ddc32e297699633f7
AbhishekR14 <aranjan0288@gmail.com>
2024-05-17 14:38:53 +0530

    Adding signin and signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,65 @@
 import { Hono } from "hono";
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
 
-const app = new Hono<{Bindings: {DATABASE_URL: string;};}>().basePath("/api/v1"); //All the paths will begin with /api/v1
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>().basePath("/api/v1"); //All the paths will begin with /api/v1
 
-app.post("/user/signup", (c) => {
+app.post("/user/signup", async (c) => {
+  const body = await c.req.json(); //Getting the body
   const prisma = new PrismaClient({
     datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-  return c.text("Hello Hono!");
+  try {
+    const responseUser = await prisma.user.create({
+      data: {
+        name: body.name,
+        email: body.email,
+        password: body.password,
+      },
+    });
+    if (responseUser) {
+      console.log("Hi");
+      const token = await sign({ id: responseUser.id }, c.env.JWT_SECRET);
+      console.log(token);
+      return c.json({ token });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Error while signing up" });
+  }
 });
 
-app.post("/user/signin", (c) => {
-  return c.text("Hello Hono!");
+app.post("/user/signin", async (c) => {
+  const body = await c.req.json();
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const foundUser = await prisma.user.findUnique({
+      where: {
+        email: body.email,
+        password: body.password,
+      },
+    });
+    if (foundUser) {
+      console.log(c.env.JWT_SECRET);
+      const token = await sign({ id: foundUser.id }, c.env.JWT_SECRET);
+      console.log(token);
+      return c.json({ token });
+    } else {
+      c.status(403);
+      return c.json({ message: "Invalid credentials. Try Again!" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ message: "Invalid credentials. Try Again!" });
+  }
 });
 
 app.post("/blog", (c) => {
