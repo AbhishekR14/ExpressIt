05d80d91de52fc927d48f1d2fb3db5bc5c0eb20d
AbhishekR14 <aranjan0288@gmail.com>
2024-05-18 13:25:30 +0530

    Added blogRoutes

--- backend/routes/blog.ts
+++ backend/routes/blog.ts
@@ -1,4 +1,6 @@
 import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 import { verify } from "hono/jwt";
 
 const blogRoute = new Hono<{
@@ -24,20 +26,170 @@ blogRoute.use("/*", async (c, next) => {
   }
 });
 
-blogRoute.post("/", (c) => {
-  return c.text("Hello Hono!");
+blogRoute.post("/", async (c) => {
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const post = await prisma.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userId,
+      },
+    });
+    return c.json({
+      message: "Post created",
+      postId: post.id,
+    });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Post could not be created" });
+  }
 });
 
-blogRoute.put("/", (c) => {
-  return c.text("Hello Hono!");
+blogRoute.put("/", async (c) => {
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: body.postId,
+        authorId: userId, //only the creater of the post can update the post
+      },
+      data: {
+        title: body.title,
+        content: body.content,
+      },
+    });
+    return c.json({
+      message: "Post updated",
+      postId: updatedPost.id,
+    });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Post could not be updated" });
+  }
 });
 
-blogRoute.get("/:id", (c) => {
-  return c.text("Hello Hono!");
+blogRoute.get("/findpost/:id", async (c) => {
+  const postId = c.req.param("id");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const foundPost = await prisma.post.findUnique({
+      where: {
+        id: postId,
+      },
+    });
+    if (foundPost) {
+      return c.json({ Post: foundPost });
+    } else {
+      c.status(403);
+      return c.json({ error: "Post could not be found" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Post could not be found" });
+  }
 });
 
-blogRoute.get("/bulk", (c) => {
-  return c.text("Hello Hono!");
+blogRoute.get("/publish/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const publishedPosts = await prisma.post.findMany({
+      where: {
+        published: true,
+      },
+    });
+    if (publishedPosts) {
+      return c.json({ Posts: publishedPosts });
+    } else {
+      c.status(403);
+      return c.json({ error: "No Posts publshed" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Errored while getting posts" });
+  }
+});
+
+blogRoute.put("/publish", async (c) => {
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: body.postId,
+        authorId: userId, //only the creater of the post can publish the post
+      },
+      data: {
+        published: true,
+      },
+    });
+    return c.json({
+      message: "Post published",
+      postId: updatedPost.id,
+    });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Post could not be published" });
+  }
+});
+
+blogRoute.put("/unpublish", async (c) => {
+  const body = await c.req.json();
+  const userId = c.get("userId");
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: body.postId,
+        authorId: userId, //only the creater of the post can Unpublished the post
+      },
+      data: {
+        published: false,
+      },
+    });
+    return c.json({
+      message: "Post Unpublished",
+      postId: updatedPost.id,
+    });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Post could not be Unpublished" });
+  }
+});
+
+blogRoute.get("/bulk", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+  try {
+    const allPosts = await prisma.post.findMany({});
+    if (allPosts) {
+      return c.json({ Posts: allPosts });
+    } else {
+      c.status(403);
+      return c.json({ error: "No Posts" });
+    }
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Errored while getting posts" });
+  }
 });
 
 export default blogRoute;
