ab4e51be4ab6f2f730b558525ab81e0bb773d514
AbhishekR14 <aranjan0288@gmail.com>
2024-05-22 14:00:18 +0530

    Added /dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Signin from "../pages/Signin";
-import Signup from "../pages/Signup";
-import Blog from "../pages/Blog";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Blog from "./pages/Blog";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
