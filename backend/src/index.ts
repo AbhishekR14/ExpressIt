a4b90eff5e76d7644d6f0a7c29cf1fd3a6f5db5a
AbhishekR14 <aranjan0288@gmail.com>
2024-05-22 16:20:11 +0530

    added cors to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from "hono";
 import userRoute from "../routes/user";
 import blogRoute from "../routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api/v1"); //All the paths will begin with /api/v1
 
+app.use(cors());
 app.route("/user", userRoute); //All the requests coming to /api/v1/user will go to userRoute
 app.route("/blog", blogRoute); //All the requests coming to /api/v1/blog will go to blogRoute
 
