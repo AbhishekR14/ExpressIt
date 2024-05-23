bb91bdd067176d09af8d612ec32d562667911dca
AbhishekR14 <aranjan0288@gmail.com>
2024-05-23 22:00:31 +0530

    CompactBlog component

---
+++ frontend/src/components/CompactBlog.tsx
@@ -0,0 +1,32 @@
+export default function CompactBlog(props: propsInterface) {
+  return (
+    <div
+      className="border rounded-lg border-black m-2 p-2"
+      onClick={props.onClick}
+    >
+      <div className="flex grid-cols-3 pt-2 ">
+        <div className="pr-2 font-semibold text-sm lg:text-base">
+          {props.postCreator}
+        </div>
+        <div className="flex pr-2 text-slate-500 text-xs lg:text-sm">|</div>
+        <div className="flex pr-2 text-slate-600 items-center text-xs lg:text-sm ">
+          {props.postDate}
+        </div>
+      </div>
+      <div className="font-bold text-lg mb-1 lg:text-2xl line-clamp-2 lg:line-clamp-1">
+        {props.postTitle}
+      </div>
+      <div className="text-base mb-3 line-clamp-3 lg:line-clamp-2">
+        {props.postDescription}
+      </div>
+    </div>
+  );
+}
+
+interface propsInterface {
+  postCreator: string;
+  postDate: string;
+  postTitle: string;
+  postDescription: string;
+  onClick: (e: any) => void;
+}
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,15 @@
+import CompactBlog from "../components/CompactBlog";
+
 export default function Home() {
-    return <>Home</>;
-  }
-  
+  return (
+    <div className="max-w-5xl">
+      <CompactBlog
+        postCreator="Abhi Ranjan"
+        postDate="Dec 13, 2024"
+        postTitle="How an Ugly Single-Page Website Makes $5,000 a Month with Affiliate Marketing line-clamp-3 lg:line-clamp-2line-clamp-3 lg:line-clamp-2line-clamp-3 lg:line-clamp-2line-clamp-3 lg:line-clamp-2"
+        postDescription="No need to create a fancy and modern website with hundreds of pages to make money online. - Making money online is the dream for man No need to create a fancy and modern website with hundreds of pages to make money online. - Making money online is the dream for man No need to create a fancy and modern website with hundreds of pages to make money online. - Making money online is the dream for man No need to create a fancy and modern website with hundreds of pages to make money online. - Making money online is the dream for man..."
+        onClick={() => {}}
+      />
+    </div>
+  );
+}
