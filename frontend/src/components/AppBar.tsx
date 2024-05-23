dcdd131fc0d62aa6d8148868456b6f694264ff6e
AbhishekR14 <aranjan0288@gmail.com>
2024-05-23 22:55:51 +0530

    Added AppBar component

---
+++ frontend/src/components/AppBar.tsx
@@ -0,0 +1,43 @@
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+export function AppBar(props: propsInterface) {
+  const navigate = useNavigate();
+  return (
+    <div className="shadow h-14 flex justify-between">
+      <div className="flex ml-4">
+        <img
+          src="../util/ExpressIt-logo.jpg"
+          className="rounded-full h-12 w-12 flex justify-center mt-1 mr-2"
+        />
+        <div className="hidden md:flex flex-col justify-center h-full">
+          ExpressIt
+        </div>
+      </div>
+      <div className="flex">
+        <div className="rounded-full h-12 w-12 bg-gray-200 flex justify-center mt-1 mr-2">
+          <div className="flex flex-col justify-center h-full text-xl">
+            {props.userName[0].toUpperCase() + props.userName[1].toUpperCase()}
+          </div>
+        </div>
+        <div className="flex flex-col justify-center h-full mr-4">
+          {props.userName}
+        </div>
+      </div>
+      <div className="flex flex-col justify-center h-full mr-4 mt-1">
+        <Button
+          name="Log Out"
+          onClick={() => {
+            navigate("/signin");
+            localStorage.removeItem("ExpressItAuthToken");
+          }}
+        />
+      </div>
+    </div>
+  );
+}
+
+interface propsInterface {
+  userName: string;
+  onClick: (e: any) => void;
+}
