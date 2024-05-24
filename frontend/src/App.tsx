6d37471b1d2703c239e232833bb084e0a19d9c38
AbhishekR14 <aranjan0288@gmail.com>
2024-05-24 19:39:30 +0530

    Adding frontend to gitpages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,11 @@ import Signup from "./pages/Signup";
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 
-export const APIwebsite: string = "http://localhost:8787/"; //https://backend.aranjan0288.workers.dev/
+export const APIwebsite: string = "https://backend.aranjan0288.workers.dev/"; //http://localhost:8787/
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/ExpressIt/"}>
       <Routes>
         <Route path="/" element={<Signup />} />
         <Route path="/signin" element={<Signin />} />
