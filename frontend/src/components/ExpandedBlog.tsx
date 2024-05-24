b4e8d97ef37624d1a0640698aff2db2c807bd3c6
AbhishekR14 <aranjan0288@gmail.com>
2024-05-24 22:07:21 +0530

    Cursor changed of ExpandedBlog

--- frontend/src/components/ExpandedBlog.tsx
+++ frontend/src/components/ExpandedBlog.tsx
@@ -1,38 +1,37 @@
 export default function ExpandedBlog(props: propsInterface) {
-    return (
-      <div className="border rounded-lg border-black m-2 p-2 hover:bg-slate-200">
-        <div className="flex grid-cols-3 pt-2 ">
-          <div className="pr-2 font-semibold text-sm cursor-default lg:text-base">
-            {props.postCreator}
-          </div>
-          <div className="flex pr-2 text-slate-500 text-xs cursor-default lg:text-sm">
-            |
-          </div>
-          <div className="flex pr-2 text-slate-600 items-center cursor-default text-xs lg:text-sm ">
-            {props.postDate}
-          </div>
+  return (
+    <div className="border rounded-lg border-black m-2 p-2 ">
+      <div className="flex grid-cols-3 pt-2 ">
+        <div className="pr-2 font-semibold text-sm cursor-default lg:text-base">
+          {props.postCreator}
         </div>
-        <div
-          className="font-bold text-lg mb-1 lg:text-2xl cursor-pointer line-clamp-2 lg:line-clamp-1"
-          onClick={props.expandBlog}
-        >
-          {props.postTitle}
+        <div className="flex pr-2 text-slate-500 text-xs cursor-default lg:text-sm">
+          |
         </div>
-        <div
-          className="text-base mb-3 line-clamp-3 cursor-pointer lg:line-clamp-2"
-          onClick={props.expandBlog}
-        >
-          {props.postDescription}
+        <div className="flex pr-2 text-slate-600 items-center cursor-default text-xs lg:text-sm ">
+          {props.postDate}
         </div>
       </div>
-    );
-  }
-  
-  interface propsInterface {
-    postCreator: string;
-    postDate: string;
-    postTitle: string;
-    postDescription: string;
-    expandBlog: (e: any) => void;
-  }
-  
+      <div
+        className="font-bold text-lg mb-1 lg:text-2xl cursor-default line-clamp-2 lg:line-clamp-1"
+        onClick={props.expandBlog}
+      >
+        {props.postTitle}
+      </div>
+      <div
+        className="text-base mb-3 line-clamp-3 cursor-default lg:line-clamp-2"
+        onClick={props.expandBlog}
+      >
+        {props.postDescription}
+      </div>
+    </div>
+  );
+}
+
+interface propsInterface {
+  postCreator: string;
+  postDate: string;
+  postTitle: string;
+  postDescription: string;
+  expandBlog: (e: any) => void;
+}
